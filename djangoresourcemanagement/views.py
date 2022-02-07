dcb3d7b9bbed8bc6bf3a2fb262a642c1f7c778e0
KellyLieske <70502565+KellyLieske@users.noreply.github.com>
2022-02-07 03:13:20 -0500

    Feat: Basic Url and Views Creation
    Created basic views and urls for starting areas of our project

--- djangoresourcemanagement/views.py
+++ djangoresourcemanagement/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .models import *
 # Create your views here.
+
+def login_page(request):
+    return render(request, 'login.html')
+
+def profile_page(request):
+    return render(request, 'profile.html')
+
+def logout_user(request):
+    return render(request, 'logout.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def project(request):
+    return render(request, 'project.html')
