fc7db8e32310fb6b212ba4c01c89a13aa744be65
ELijah Smith <esmit147@emich.edu>
2022-02-17 13:02:54 -0500

    Feat: Log in capabilities
    Established the ability to log in using users from the database and the ability to log out using
    page url. Specifically added 2 views (login_page and logout_user).

--- djangoresourcemanagement/views.py
+++ djangoresourcemanagement/views.py
@@ -4,13 +4,28 @@ from .models import *
 # Create your views here.
 
 def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('profile')
+
+    if request.method == 'POST':
+        username = request.POST['login_username']
+        password = request.POST['login_password']
+        user = authenticate(request, username = username, password = password)
+        print("auth")
+        if user is not None:
+            login(request, user)
+            print("log")
+            return redirect('profile')
+
     return render(request, 'login.html')
 
 def profile_page(request):
     return render(request, 'profile.html')
 
 def logout_user(request):
-    return render(request, 'logout.html')
+    if request.user.is_authenticated:
+        logout(request)
+        return redirect('login')
 
 def team(request):
     return render(request, 'team.html')
