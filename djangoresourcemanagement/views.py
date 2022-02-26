19b3e1eae9fcdbe4214abf6b64dac0e587b6641a
KellyLieske <70502565+KellyLieske@users.noreply.github.com>
2022-02-26 02:41:47 -0500

    Ref: Creates Dictionary of Regex strings
    Creates a dictionary of regex strings instead of checking inline, utilizes self descriptive variable names to help with documentation

--- djangoresourcemanagement/views.py
+++ djangoresourcemanagement/views.py
@@ -158,9 +158,18 @@ def import_users(request):
     #print(request.method)
     #print(request.FILES.keys())
     # Next three lines are for testing demo purposes, remove at deploy
-    #user = authenticate(request, username = 'jack123', password = 'cosc481w')
+    user = authenticate(request, username = 'jack123', password = 'cosc481w')
     # user = authenticate(request, username='jimboTheBro', password='cosc481w')
-    #login(request, user)
+    login(request, user)
+    RegexStrings = {
+        'Password' : '^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$',
+        'Email' : '\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}\\b',
+        'NumbersAndSpecialCharacters' : "^[0-9<>%$]{2,}$",
+        'ValidDate' : "^(0[1-9]|1[0-2])[-/](0[1-9]|[12][0-9]|3[01])[-/](18|19|20)\\d\\d$",
+        'SpecialCharacters' : '^[<>%$]{2,}$',
+        'LettersAndSpecialCharacters' : '^[a-zA-Z<>%$]{2,}$',
+
+    }
     if not request.user.is_authenticated or request.user.permission == 'EMP' or request.user.permission == 'LEAD':
         raise PermissionDenied
     if request.method == 'POST' and 'userfile' in request.FILES:
@@ -182,36 +191,36 @@ def import_users(request):
 
             if (len(row) < 2 or row[1] == ""):                                          # Password
                 invalidindicies.append((count, 2, 'Password cannot be empty'))
-            elif not re.match("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$", str(row[1])):
+            elif not re.match(RegexStrings['PasswordRegex'], str(row[1])):
                 invalidindicies.append((count,  2, 'Password must contain at least 8 characters, at least 1  uppercase letter 1 lower case letter and 1 number'))
             else:
                 userdata[count].append(row[1])
 
             if (len(row) < 3 or row[2] == ""):                                          # Email
                 invalidindicies.append((count, 3, 'Email cannot be empty'))
-            elif  not re.match("\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}\\b", row[2]):
+            elif  not re.match(RegexStrings['EmailRegex'], row[2]):
                 invalidindicies.append((count,  3, "Email must be valid"))
             else:
                 userdata[count].append(row[2])
 
             if (len(row) < 4 or row[3] == ""):
                 invalidindicies.append((count, 4, 'First Name cannot be empty'))
-            elif re.match("^[0-9<>%$]{2,}$", row[3]):
+            elif re.match(RegexStrings['NumbersAndSpecialCharactersRegex'], row[3]):
                 invalidindicies.append((count, 4, "First Name cannot have numbers or <>%$"))
             else:
                 userdata[count].append(row[3])
 
             if (len(row) < 5 or row[4] == ""):
                 invalidindicies.append((count, 4, 'Last Name cannot be empty'))
-            elif re.match("^[0-9<>%$]{2,}$", row[4]):
+            elif re.match(RegexStrings['NumbersAndSpecialCharactersRegex'], row[4]):
                 invalidindicies.append((count, 5, "Last name cannot have numbers or <>%$"))
             else:
                 userdata[count].append(row[4])
 
             if (len(row) < 6 or row[5] == ""):
                 invalidindicies.append((count, 6, 'HireDate cannot be empty'))
-            elif not re.match("^(0[1-9]|1[0-2])[-](0[1-9]|[12][0-9]|3[01])[-](18|19|20)\\d\\d$", row[5]):
-                invalidindicies.append((count, 6, 'Improperly formatted date, must be in mm-dd-yyyy'))
+            elif not re.match(RegexStrings['ValidDate'], row[5]):
+                invalidindicies.append((count, 6, 'Improperly formatted date, must be in mm-dd-yyyy or mm/dd/yyyy'))
             elif not helper.valid_date(row[5]):
                 invalidindicies.append(count, 6, "Date submitted is in correct format but invalid, (Eg. >32 days, 31 days in a month with 30, Before year 1800)")
             else:
@@ -219,28 +228,28 @@ def import_users(request):
 
             if (len(row) < 7 or row[6] == ""):
                 userdata[count].append(None)
-            elif re.match("^[<>%$]{2,}$", row[6]):
+            elif re.match(RegexStrings['SpecialCharacters'], row[6]):
                 invalidindicies.append((count, 7, "Address cannot have <>%$"))
             else:
                 userdata[count].append(row[6])
 
             if (len(row) < 8 or row[7] == ""):
                 userdata[count].append(None)
-            elif re.match("^[0-9<>%$]{2,}$", row[7]):
+            elif re.match(RegexStrings['NumbersAndSpecialCharacters'], row[7]):
                 invalidindicies.append((count, 8, "Position cannot have <>%$ or numbers"))
             else:
                 userdata[count].append(row[7])
 
             if (len(row) < 9 or row[8] == ""):
                 userdata[count].append(None)
-            elif re.match("^[0-9<>%$]{2,}$", row[8]):
+            elif re.match(RegexStrings['NumbersAndSpecialCharacters'], row[8]):
                 invalidindicies.append((count, 9, "Marital Status cannot have numbers or <>%$"))
             else:
                 userdata[count].append(row[8])
 
             if (len(row) < 10 or row[9] == ""):
                 userdata[count].append(None)
-            elif re.match("^[a-zA-Z<>%$]{2,}$", str(row[9])):
+            elif re.match(RegexStrings['LettersAndSpecialCharacters'], str(row[9])):
                 invalidindicies.append((count, 10, "Rate cannot have letters or <>%$"))
             else:
                 userdata[count].append(row[9])
@@ -256,8 +265,6 @@ def import_users(request):
 
             if (len(row) < 12 or row[11] == ""):
                 userdata[count].append(None)
-            elif re.match("^[0-9<>%$]{2,}$", row[11]):
-                invalidindicies.append((count, 12, "Mentor cannot have numbers or <>%$"))
             elif not Users.objects.all().filter(username=row[11]).exists():
                 invalidindicies.append((count, 12, "User by that username does not exist"))
             else:
