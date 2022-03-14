f0833247204c1c95e557a0f40b4c258ad47e46b5
Brian Simmons <bsimmo11@emich.edu>
2022-03-14 16:40:50 -0400

    feat: Async javascript call to get pending skills
    Created javascript file that grabs the pending skills from
    the nav/getNotifications url.

--- static/js/notifications/nav_notification.js
+++ static/js/notifications/nav_notification.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
+    $("#notification_section").on("click", function(){
+        $.ajax({
+            type: "GET",
+            url: "getNotifications",
+            success: function(data){
+                updateNotificationCount(data);
+            },
+            dataType: "json"
+        })
+    })
 
-
-
+    function updateNotificationCount(data)
+    {
+        let count = data.suggestions.length;
+        console.log($("#notification_count"));
+        $("#notification_count")[0].innerText = count;
+    }
 
 })
