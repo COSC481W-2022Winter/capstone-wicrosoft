269eb2f9df2d6bb7f48562b79c86a4b3269fe54d
KellyLieske <70502565+KellyLieske@users.noreply.github.com>
2022-03-14 06:45:50 -0400

    feat: Add color coordinated indicator for skill approval status
    Adds a color coordiated circle to each row to show the status of each skill, App is green, pen is yellow, rej is red

---
+++ static/js/addskills/skills_addskills.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+    let skillstable = document.getElementById("skillsTable");
+
+     let skillstablebody = skillstable.children[1];
+    console.log(skillstablebody.tagName);
+
+
+
+
+    for(let i = 0; i < skillstablebody.children.length; i++) {
+        let row = skillstablebody.children[i];
+        let rowStatus = row.children[2];
+
+            // console.log(rowStatus.textContent);
+            // let circle1 = document.createElement("div");
+            // circle1.setAttribute("class" ,"approvedCircle");
+            // rowStatus.appendChild(circle1);
+
+
+        if (rowStatus.textContent === 'APP') {
+            let circle1 = document.createElement("div");
+            circle1.setAttribute("class" ,"approvedCircle");
+            rowStatus.appendChild(circle1);
+        }
+        else if (rowStatus.textContent === 'PEN') {
+           // console.log('got here')
+            rowStatus.
+            let circle1 = document.createElement("div");
+            circle1.setAttribute("class" ,"pendingCircle");
+            rowStatus.appendChild(circle1);
+        }
+        else if (rowStatus.textContent === "REJ") {
+            let circle1 = document.createElement("div");
+            circle1.setAttribute("class" ,"rejectedCircle");
+            rowStatus.appendChild(circle1);
+        }
+
+    }
+
+} );
