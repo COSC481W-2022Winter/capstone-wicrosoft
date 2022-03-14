dae7cebbcd6e829e47fee2da836aba3179d42f03
Brian Simmons <bsimmo11@emich.edu>
2022-03-14 16:05:24 -0400

    refactor: added nav view for testing

--- capstone_wicrosoft/urls.py
+++ capstone_wicrosoft/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     path('teammaker/get_projects/', views.get_projects),
     path('project/', views.project, name='project'),
     path('importusers/', views.import_users, name='import'),
+    path('nav/', views.nav)
 
 ]
