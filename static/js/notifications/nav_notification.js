26ec578b2523373913dcfc2f9ff4ffbb15c6546d
Brian Simmons <bsimmo11@emich.edu>
2022-03-15 12:57:22 -0400

    feat: Added code for timed function call
    The notifications will be queried every 5 seconds and the page
    will be updated accordingly.

--- static/js/notifications/nav_notification.js
+++ static/js/notifications/nav_notification.js
@@ -1,20 +1,32 @@
 $(document).ready(function(){
-    $("#notification_section").on("click", function(){
+    ajaxGetNotifications();
+    setInterval(ajaxGetNotifications, 5000);
+
+    function updateNotificationCount(data)
+    {
+        console.log(data);
+        let count = data.suggestions.length;
+        if (count === 0)  { return; }
+
+        $(".notification_indicator").each(function(){
+            $( this ).removeClass("d-none");
+        })
+        $("#notification_section").removeClass("d-none");
+        $("#notification_count")[0].innerText = count;
+        $("#skill_notification_count").removeClass("d-none");
+        $("#skill_notification_count")[0].innerText = count;
+    }
+
+    function ajaxGetNotifications()
+    {
         $.ajax({
             type: "GET",
-            url: "getNotifications",
+            url: "/getNotifications",
             success: function(data){
                 updateNotificationCount(data);
             },
             dataType: "json"
         })
-    })
-
-    function updateNotificationCount(data)
-    {
-        let count = data.suggestions.length;
-        console.log($("#notification_count"));
-        $("#notification_count")[0].innerText = count;
     }
 
 })
